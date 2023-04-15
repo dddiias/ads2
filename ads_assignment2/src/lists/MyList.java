d58243c763f708b43102faae2480c36ccebe6011
dddiias <115692320+dddiias@users.noreply.github.com>
2023-04-15 16:55:55 +0600

    Add files via upload

---
+++ ads_assignment2/src/lists/MyList.java
@@ -0,0 +1,15 @@
+package lists;
+
+public interface MyList <T>{
+    int size();
+    boolean contains(Object o);
+    void add(T item);
+    void add(T item, int index);
+    boolean remove(T item);
+    T remove(int index);
+    void clear();
+    T ger(int index);
+    int indexOf(Object o);
+    int lastIndexOf(Object o);
+    void sort();
+}
